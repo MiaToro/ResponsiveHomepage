e7ee4e8bd49545e403c7c7dbfe9f7e30cb50f54d
Mia Jiao <sq.jiao@hotmail.com>
2018-12-30 20:08:41 -0500

    Add files via upload

--- js/marketcircle.js
+++ js/marketcircle.js
@@ -1,1 +1,18 @@
+/*  
+ */
 
+$(document).ready(function () { 
+    
+    $("#menu-pop").hide();
+    
+    $("#burger a").click(function (e) {
+        e.preventDefault();
+   
+        if ($("#menu-pop").css("display") == "none") { 
+            $("#menu-pop").show(); 
+        } else {
+            $("#menu-pop").hide();
+        }
+    });
+
+})
